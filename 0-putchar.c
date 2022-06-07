fd2b0762c47029209aaba3243a6e4b5d54cf9a5e
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-07 04:58:32 -0700

    Task 0

---
+++ 0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Main - entry that print putchar
+ *
+ * And followed by a new line
+ *
+ * RETURN : Alwasy 0 (Successful)
+ */
+int main(void)
+{
+	printf("_putchar\n",n);
+	Return 0;
+}
