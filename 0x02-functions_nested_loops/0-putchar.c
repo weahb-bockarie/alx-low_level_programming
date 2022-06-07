5105e0332ac2830dd5d48713f797f0880d7bf238
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-07 05:49:39 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - entry print _putchar
+ *
+ * folloewd by a new line
+ *
+ * Return: Always 0 (Successful)
+ */
+ int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
