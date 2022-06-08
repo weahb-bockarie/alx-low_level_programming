c6ab8560e89f96f3b35bc506a472fe6f67388e99
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-08 11:16:30 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that add two intergers and returens
+ * @one: first number input
+ * @two: second number input
+ * Return: one + two
+ */
+int add(int  one , int two);
+{
+	return(one + two);
+}
