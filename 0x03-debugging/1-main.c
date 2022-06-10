499828e825a15dc9875b6b967c672b178816c5ff
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-10 08:21:36 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - course an infinite loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop comming:(\n");
+
+	i =0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i)
+	 * }
+	 */
+	printf("infinite loop avoided! //o\n");
+
+	return (0);
+}
