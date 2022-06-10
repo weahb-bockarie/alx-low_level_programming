81196e4464c157563a726a2c23278443ed00333a
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-10 07:55:23 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test function that print if intergers positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
