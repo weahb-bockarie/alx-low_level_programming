7bc3fff00e4de22b70b5cf51b92167dea9c4151f
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-13 14:25:40 -0700

    task 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,8 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do{
+	do
+	{
 		if (*s == '-')
 			sign *= -1;
 
@@ -20,8 +21,7 @@ int _atoi(char *s)
 
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (num * sign);
 }
