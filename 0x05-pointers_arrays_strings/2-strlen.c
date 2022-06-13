283d4061152cb271924b0f96a6dbe377f3f495d2
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-13 12:13:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of the string
+ *@str: string
+ *
+ * return : length
+ */
+size_t _strlen(const char *str)
+{
+	size_t_len = 0;
+
+	while(*str**)
+		len++;
+
+	return (len);
+}
