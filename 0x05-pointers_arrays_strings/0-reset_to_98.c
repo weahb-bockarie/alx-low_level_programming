d66394d12be4d87647814d52193e3ea435d94259
weahb-bockarie <weahbockarie89@gmail.com>
2022-06-13 11:13:34 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset value of pointdr to 98
+ * reset_to_98 - resets value of pointers to 98
  * @n: pointer to reset to 98
  *
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdlib.h>
 
-int _pitchar(char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,7 +10,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void puts_half(char *str);
+oid puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
